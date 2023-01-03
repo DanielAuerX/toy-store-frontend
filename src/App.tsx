a1633c06f2e3b3cc7329a4050b387f983bd8e4bf
Auer <D.Auer@eos-ts.com>
2023-01-03 16:08:15 +0100

    changed title

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export interface IState {
 }
 
 function App(){
-    document.title = 'EOS Toy Store'
+    document.title = 'äosTECH'
 
 
     const [toys, setToys] = useState<IState["toys"]>([])
@@ -39,7 +39,7 @@ function App(){
     return (
         <div className="App">
             <img src={logo} alt="Logo" style={{ width: '350px', height: '180px' }} />
-            <h1>Toy Store</h1>
+            <h1>.:Toy Store:.</h1>
             <h2>Toys in the inventory</h2>
             <List toys={toys}/>
             <AddToList setToys={setToys} toys={toys}/>
